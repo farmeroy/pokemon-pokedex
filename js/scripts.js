992db7ebb2be71151c971cde3eab20874d2dd460
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-01 15:43:12 +0100

    changed pokemon__name to pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -20,12 +20,14 @@ pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++) {
   const pokemon = pokemonList[i];
+  // conditionally comment on the Pokemon's height using a ternary expression
   let comment =
     pokemon.height > 1
       ? '<p class="pokemon__comment">Wow! That is BIG!!!</p>'
       : "";
+      
   document.write(
-    `<div class="pokemon__name">
+    `<div class="pokemon">
         <h1>${pokemon.name}</h1>
         <p class="pokemon__value">Height: ${pokemon.height}m</p>
         ${comment}
