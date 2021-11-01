9e0f78802923ba50500d1685f796ef2c9c068af4
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-01 11:48:02 +0100

    added pokemonList data

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+let pokemonList = [];
+
+pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Venusaur",
+    height: 2,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Chinchou",
+    height: 0.5,
+    types: ["electric", "water"],
+  },
+];
