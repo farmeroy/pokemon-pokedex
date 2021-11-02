db3ce5a175b0301cc3d007a27f1cf7a1659aa49c
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-02 10:58:44 +0100

    wrapped pokemonList in IIFE, added methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,59 @@
-let pokemonList = [];
+const pokemonRepository = (function () {
+  
+  let pokemonList = [];
 
-pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Venusaur",
-    height: 2,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Chinchou",
-    height: 0.5,
-    types: ["electric", "water"],
-  },
-];
+  pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Venusaur",
+      height: 2,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Chinchou",
+      height: 0.5,
+      types: ["electric", "water"],
+    },
+  ];
+
+  const add = function (pokemon) {
+    if (
+      typeof pokemon !== Object ||
+      Object.keys(pokemon) !== ["name", "height", "types"] ||
+      typeof pokemon.types !== Array
+    ) {
+      return console.log(
+        "pokemon must be an object with keys name, height, types (array)"
+      );
+    }
+
+    pokemonList.push(pokemon);
+  };
+
+  const find = function (name) {
+    return pokemonList.filter(function (pokemon, name) {
+      pokemon.name === name;
+    });
+  };
+
+  const getAll = function () {
+    return pokemonList;
+  };
+
+  return {
+    getAll: getAll,
+    add: add,
+    find: find,
+  };
+})();
+
 
 function printPokemon(pokemon) {
-
+ 
   let comment =
     pokemon.height > 1
       ? '<p class="pokemon__comment">Wow! That is BIG!!!</p>'
@@ -34,4 +68,4 @@ function printPokemon(pokemon) {
   );
 }
 
-pokemonList.forEach(printPokemon)
+pokemonRepository.getAll().forEach(printPokemon);
