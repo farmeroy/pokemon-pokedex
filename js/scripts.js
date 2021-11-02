34b62abf905dcaa363299b625744c3589d63260b
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-02 14:20:29 +0100

    added showDetails eventListener

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ const pokemonRepository = (function () {
   ];
 
   const add = function (pokemon) {
+    //error handling
     const keys = Object.keys(pokemon);
     if (keys[0] !== "name" || keys[1] !== "height" || keys[2] !== "types") {
       return console.log(
@@ -34,22 +35,32 @@ const pokemonRepository = (function () {
     pokemonList.push(pokemon);
   };
 
+  //search for a pokemon by name
   const find = function (name) {
     return pokemonList.filter(function (pokemon, name) {
       pokemon.name === name;
     });
   };
 
+  //returns a list of all the pokemon
   const getAll = function () {
     return pokemonList;
   };
 
+  // log a pokemon's name to the console
+  const showDetails = function (pokemon) {
+    console.log(pokemon.name);
+  };
+
+  //renders a pokemon object to the DOM
   const addListItem = function (pokemon) {
     const ulElement = document.querySelector(".pokemon-list");
     const listElement = document.createElement("li");
     const pokemonButton = document.createElement("button");
 
     pokemonButton.innerText = pokemon.name;
+    pokemonButton.addEventListener("click", showDetails.bind(null, pokemon));
+
     pokemonButton.classList.add("pokemon");
     listElement.classList.add("pokemon-list__item");
 
@@ -61,9 +72,8 @@ const pokemonRepository = (function () {
     getAll: getAll,
     add: add,
     find: find,
-    addListItem: addListItem
+    addListItem: addListItem,
   };
 })();
-
 
 pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
