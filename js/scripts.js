4867763433a32626025b4547c4e4797ce7f3ec4e
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-02 09:48:04 +0100

    refactored printPokemonList to printPokemon with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -18,24 +18,20 @@ pokemonList = [
   },
 ];
 
-function printPokemonList(list) {
-    
-  for (let i = 0; i < list.length; i++) {
-    const pokemon = list[i];
-    // conditionally comment on the Pokemon's height using a ternary expression
-    let comment =
-      pokemon.height > 1
-        ? '<p class="pokemon__comment">Wow! That is BIG!!!</p>'
-        : "";
+function printPokemon(pokemon) {
 
-    document.write(
-      `<div class="pokemon">
+  let comment =
+    pokemon.height > 1
+      ? '<p class="pokemon__comment">Wow! That is BIG!!!</p>'
+      : "";
+
+  document.write(
+    `<div class="pokemon">
             <h1>${pokemon.name}</h1>
             <p class="pokemon__value">Height: ${pokemon.height}m</p>
             ${comment}
         </div>`
-    );
-  }
+  );
 }
 
-printPokemonList(pokemonList)
+pokemonList.forEach(printPokemon)
