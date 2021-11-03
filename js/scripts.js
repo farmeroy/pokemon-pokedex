32c5041638e62fa27f5e20524258263de8ef1f41
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-03 10:43:45 +0100

    added fetch request, add function error handling must be updated

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,40 @@
 
 const pokemonRepository = (function () {
   let pokemonList = [];
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-  pokemonList = [
-    {
-      name: "Bulbasaur",
-      height: 0.7,
-      types: ["grass", "poison"],
-    },
-    {
-      name: "Venusaur",
-      height: 2,
-      types: ["grass", "poison"],
-    },
-    {
-      name: "Chinchou",
-      height: 0.5,
-      types: ["electric", "water"],
-    },
-  ];
+  // pokemonList = [
+  //   {
+  //     name: "Bulbasaur",
+  //     height: 0.7,
+  //     types: ["grass", "poison"],
+  //   },
+  //   {
+  //     name: "Venusaur",
+  //     height: 2,
+  //     types: ["grass", "poison"],
+  //   },
+  //   {
+  //     name: "Chinchou",
+  //     height: 0.5,
+  //     types: ["electric", "water"],
+  //   },
+  // ];
 
   const add = function (pokemon) {
-    //error handling
-    const keys = Object.keys(pokemon);
-    if (keys[0] !== "name" || keys[1] !== "height" || keys[2] !== "types") {
-      return console.log(
-        "pokemon must be an object with keys name, height, types (array)"
-      );
-    }
-    if (typeof pokemon !== "object" || typeof pokemon.types !== "object") {
-      return console.log(
-        "pokemon must be an object with keys name, height, types (array)"
-      );
-    }
+    
+    //error handling must be updated
+    // const keys = Object.keys(pokemon);
+    // if (keys[0] !== "name" || keys[1] !== "height" || keys[2] !== "types") {
+    //   return console.log(
+    //     "pokemon must be an object with keys name, height, types (array)"
+    //   );
+    // }
+    // if (typeof pokemon !== "object" || typeof pokemon.types !== "object") {
+    //   return console.log(
+    //     "pokemon must be an object with keys name, height, types (array)"
+    //   );
+    // }
     pokemonList.push(pokemon);
   };
 
@@ -70,12 +72,40 @@ const pokemonRepository = (function () {
     ulElement.appendChild(listElement);
   };
 
+  const loadList = function () {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  };
+
   return {
     getAll: getAll,
     add: add,
     find: find,
     addListItem: addListItem,
+    loadList: loadList
   };
 })();
 
-pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
+
+
+
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
+});
